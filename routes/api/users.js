06ba514660722d7135fb3c61fc349c503c4f2ea6
Jasmine Dang <ngandth.1992@gmail.com>
2020-07-20 23:05:20 +0800

    added validation for user model

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
-
-// @route   GET api/users
+const { check, validationResult } = require('express-validator');
+// @route   POST api/users
 // @desc    Test route
 // @@access Public
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is require').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password must be at least 6 letters').isLength({
+      min: 6,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors.array());
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('User route');
+  }
+);
 
 module.exports = router;
